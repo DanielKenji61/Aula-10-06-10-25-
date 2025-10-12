067e91c8dbe1dbeaf6b608274be9ea10ef0bd926
DanielKenji61 <danielhayassy@gmail.com>
2025-10-12 19:39:08 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,7 +8,13 @@ nome = st.text_input("Digite o seu nome:")
 cor = st.selectbox("Escolha a cor do texto:", ["blue", "red", "green", "purple", "orange"])
 
 if nome:
+    # Nome em maiúsculas com a cor escolhida
     st.markdown(
         f'<p style="color:{cor}; font-size:24px;">{nome.upper()}</p>',
+        unsafe_allow_html=True
+    )
+
+    st.markdown(
+        f'<p style="color:{cor}; font-size:20px;">Alô mundo, meu nome é {nome}</p>',
         unsafe_allow_html=True
     )

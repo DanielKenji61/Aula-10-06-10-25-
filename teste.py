37c2f029097783f42fb3df3146d76dde9bdd3a6e
DanielKenji61 <danielhayassy@gmail.com>
2025-10-12 19:37:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,17 +4,11 @@ st.title("Meu programa")
 st.write("Alô mundo")
 
 nome = st.text_input("Digite o seu nome:")
-if nome:
-  st.write(nome.upper())
-
-import streamlit as st
-
-st.title("Meu programa")
-st.write("Alô mundo")
-
-nome = st.text_input("Digite o seu nome:")
 
 cor = st.selectbox("Escolha a cor do texto:", ["blue", "red", "green", "purple", "orange"])
 
 if nome:
-    st.markdown(f'<p style="color:{cor}; font-size:24px;">{nome.upper()}</p>', unsafe_allow_html=True)
+    st.markdown(
+        f'<p style="color:{cor}; font-size:24px;">{nome.upper()}</p>',
+        unsafe_allow_html=True
+    )

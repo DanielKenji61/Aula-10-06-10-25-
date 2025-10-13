2bc36a48ac695b00c73feb474fc6e99782a588ca
DanielKenji61 <danielhayassy@gmail.com>
2025-10-13 15:18:30 -0300

    Update PAÍSES.py

--- PAÍSES.py
+++ PAÍSES.py
@@ -7,8 +7,8 @@ dataset = pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
 fig = px.choropleth(dataset,
                     locations=dataset['iso3'],
                     color=dataset['longitude'],
-                    hover_name=dataset['nome])
+                    hover_name=dataset['nome'])
 
 fig.update_layout(tittle='Mapa Coroplético dos Países', geo_scope='world')
 
-st.plotly_chart(fig, use_container_width=True, these ="streamlit")
+st.plotly_chart(fig, use_container_width=True, theme ="streamlit")
